ca1419a95265b175217de6ca2db184dfd59c365c
An <an.tran511@outlook.com>
2025-03-22 16:43:21 +0700

    feat: add mantine and tailwind css

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,12 @@
 import { Outlet, createRootRoute } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
+import { MantineProvider } from '@mantine/core';
 
 export const Route = createRootRoute({
   component: () => (
-    <>
+    <MantineProvider>
       <Outlet />
       <TanStackRouterDevtools />
-    </>
+    </MantineProvider>
   ),
 });
